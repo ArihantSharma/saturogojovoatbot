0519db3771571d583bb7f365ad4dd008cdb6ffe6
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-03-20 07:02:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://saturogojovoat:Luv@2Arihant@cluster0.ccaf3g6.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "saturogojovoat")
+DB_NAME = os.environ.get("DATABASE_NAME", "filesharebot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
